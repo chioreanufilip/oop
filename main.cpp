064c3a987df88aa47a84c5c0062b194d52230676
Chioreanu Filip-Andrei <chioreanufilip@gmail.com>
2024-03-07 13:01:48 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-
+#include "test.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    ha  corut;
+    corut.aha = "ura";
+    corut.eda();
+
     return 0;
 }
---
+++ test.h
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+//using namespace std
+class ha {
+public:
+    int hula;
+    std::string aha;
+void  eda(){
+    std::cout<<"shakom";
+};
+};//
+// Created by chior on 07.03.2024.
+//
+
+#ifndef OOP_TEST_H
+#define OOP_TEST_H
+
+#endif //OOP_TEST_H
